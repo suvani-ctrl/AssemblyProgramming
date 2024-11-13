bc1f916303eed84dfbd6f352cc13911e7823c445
suvani-ctrl <sssubbbi1234@gmail.com>
2024-11-13 13:54:54 +0545

    Create Strings1.asm
    Strings code in asm

---
+++ Strings1.asm
@@ -0,0 +1,19 @@
+section .data
+
+        string1 DB "ABA",0
+        string2 DB "CDE",0
+                
+
+section .text
+
+global _start
+
+
+_start:
+        MOV bl,[string1]
+        MOV eax,1
+        INT 80h
+
+
+
+
